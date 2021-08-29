4d208f3068432e6d277b386fd0e328aa86ca4064
dubem <dubemokam@gmail.com>
2021-08-29 05:37:03 -0700

    fixed

---
+++ Develop/client/build/precache-manifest.7dd6b0c314f494a6946a2fd5611006c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8d3bd537b9e4972302d687c567629fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3319d5ce3a323486e054",
+    "url": "/static/css/2.fa6c921b.chunk.css"
+  },
+  {
+    "revision": "12436866c968f2d9f3c8",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "3319d5ce3a323486e054",
+    "url": "/static/js/2.81c7b19d.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.81c7b19d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12436866c968f2d9f3c8",
+    "url": "/static/js/main.1c084d02.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
